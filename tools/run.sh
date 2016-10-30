561d781f4d48ad10ff3747b6a326b843d03b1592
Marcus Henry Ewert <sarum90@gmail.com>
2016-10-29 19:04:07 -0700

    some things

--- tools/run.sh
+++ tools/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#docker run --rm -v `pwd`:/repo travis:default /repo/tools/run_in_container.sh
+docker run --rm -v `pwd`:/repo travis:default /repo/tools/run_in_container.sh
 
-docker run --rm -it -v `pwd`:/repo travis:default /bin/bash
+#docker run --rm -it -v `pwd`:/repo travis:default /bin/bash
 
--- tools/travis_make_deps.sh
+++ tools/travis_make_deps.sh
@@ -19,11 +19,11 @@ GSUTIL=./build/root/native/gcloud/bin/gsutil
 
 $GCLOUD auth activate-service-account --key-file=./travis-secrets/decrypted/mewert-cpp-project-6899912be999.json
 
-sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
-sudo -E apt-get -yq update &>> ~/apt-get-update.log
-sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install gcc-4.8 g++-4.8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
-gcc --version && g++ --version
+#sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
+#sudo -E apt-get -yq update &>> ~/apt-get-update.log
+#sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install gcc-4.8 g++-4.8
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
+#gcc --version && g++ --version
 make cmake
 #export PATH=$PWD/build/root/native/cmake/bin:$PATH
 #cmake --version
@@ -32,5 +32,5 @@ make cmake
 #clang++ --version
 #make deps
 
-(cd build && tar cvzf ${DEP_HASH}.tar.gz)
+(cd build && tar cvzf ${DEP_HASH}.tar.gz root)
 $GSUTIL cp -a public-read build/${DEP_HASH}.tar.gz gs://mewert-cpp-project-test-resources/deps/${DEP_HASH}.tar.gz
