315245227f8fae9dbaf993ddc928be7fb45570c8
Marcus Henry Ewert <sarum90@gmail.com>
2016-10-30 14:24:41 -0700

    travis test

--- tools/run_travis.sh
+++ tools/run_travis.sh
@@ -7,10 +7,12 @@ set -e
 
 ./tools/get_deps.sh
 
-ls ./build/root/native/cmake
+sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
+sudo -E apt-get -yq update &>> ~/apt-get-update.log
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install gcc-4.8 g++-4.8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
+
 
 export PATH=$PWD/build/root/native/cmake/bin:$PATH
 source build/root/native/emsdk/emsdk_env.sh
-
-clang++ --version && make -C modules/mestring/ && ./modules/mestring/test_mestring && echo "PASSED!"
 
