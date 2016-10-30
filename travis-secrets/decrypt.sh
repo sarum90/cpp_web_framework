970e0f2453972e99bd5df223fa125d2832c94355
Marcus Henry Ewert <sarum90@gmail.com>
2016-10-29 17:17:34 -0700

    more secrets

--- travis-secrets/decrypt.sh
+++ travis-secrets/decrypt.sh
@@ -10,10 +10,11 @@ fi
 
 mkdir -p decrypted/
 
-for F in `cd encrypted/ && ls`
+for F in `cd encrypted/ && ls | grep -v '_aeskey_'`
 do
   echo Decrypting... $F
-  openssl rsautl -decrypt -inkey private_key/private_key.pem -in encrypted/$F -out decrypted/$F
+  openssl rsautl -decrypt -inkey private_key/private_key.pem -in encrypted/$F._aeskey_ -out decrypted/$F._aeskey_
+  openssl enc -d -aes-256-cbc -out decrypted/$F -in encrypted/$F -pass file:decrypted/$F._aeskey_
   echo Done
 done
 
--- travis-secrets/encrypt.sh
+++ travis-secrets/encrypt.sh
@@ -3,9 +3,13 @@
 set -e
 set -u
 
-for F in `cd decrypted/ && ls`
+mkdir -p encrypted
+
+for F in `cd decrypted/ && ls | grep -v '_aeskey_'`
 do
   echo Encrypting... $F
-  openssl rsautl -encrypt -inkey public_key.pem -pubin -in decrypted/$F -out encrypted/$F
+  openssl rand 245 > decrypted/$F._aeskey_
+  openssl rsautl -encrypt -inkey public_key.pem -pubin -in decrypted/$F._aeskey_ -out encrypted/$F._aeskey_
+  openssl enc -aes-256-cbc -in decrypted/$F -out encrypted/$F -pass file:decrypted/$F._aeskey_
   echo Done
 done
