b8b2341ac3b2dba0a56c35d41fe4441c2f2d70d2
Marcus Henry Ewert <sarum90@gmail.com>
2016-10-31 23:42:53 -0700

    update again

--- tools/run_travis.sh
+++ tools/run_travis.sh
@@ -5,13 +5,10 @@ set -e
 
 (cd travis-secrets/ && ./decrypt.sh)
 
-apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
-apt-get -yq update &>> ~/apt-get-update.log
-apt-get -yq --no-install-suggests --no-install-recommends --force-yes install gcc-4.8 g++-4.8
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
-
 ./tools/get_deps.sh
 
 export PATH=$PWD/build/root/native/cmake/bin:$PATH
 source build/root/native/emsdk/emsdk_env.sh
 
+
+make -C modules/mestring/
