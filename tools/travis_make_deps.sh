2c6429ff1b7cdd1152ac6b426da1b31031bfb3d4
Marcus Henry Ewert <sarum90@gmail.com>
2016-11-04 22:32:15 -0700

    update g++ all the way

--- tools/travis_make_deps.sh
+++ tools/travis_make_deps.sh
@@ -21,8 +21,8 @@ $GCLOUD auth activate-service-account --key-file=./travis-secrets/decrypted/mewe
 
 sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
 sudo -E apt-get -yq update &>> ~/apt-get-update.log
-sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install gcc-4.8 g++-4.8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install gcc-6 g++-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 gcc --version && g++ --version
 make cmake
 export PATH=$PWD/build/root/native/cmake/bin:$PATH
