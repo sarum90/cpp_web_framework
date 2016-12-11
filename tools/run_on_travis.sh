b71493e073df2774953b40ec4fea4886675e5aff
Marcus Henry Ewert <user@marcushenryewert.com>
2016-12-11 19:53:07 +0000

    wip

--- tools/run_on_travis.sh
+++ tools/run_on_travis.sh
@@ -7,7 +7,7 @@ set -e
 export PATH=`pwd`/build/root/native/cmake/bin:$PATH
 source ./build/root/native/emsdk/emsdk_env.sh
 
-make -C modules/mestring/ test
+./tools/wrapper.sh make -C modules/mestring/ test
 bash
 make -C modules/webserver/ test
 make -C modules/reax/ test
