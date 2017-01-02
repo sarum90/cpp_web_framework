bff881ad8ed4d0a22e2fd3bc97a8f0fda35d677a
Marcus Henry Ewert <sarum90@gmail.com>
2017-01-02 13:59:04 -0800

    wip

---
+++ modules/descbuild/target.capnp
@@ -0,0 +1,9 @@
+@0x982cf519993b5447; 
+
+struct CppTarget {
+  name @0 :Text;
+  headers @1 :List(Text);
+  sources @2 :List(Text);
+  dependencies @3 :List(Text);
+}
+
---
+++ modules/descbuild/test.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+#include "yaml-cpp/yaml.h"
+
+int main(int argc, char ** argv) {
+  YAML::Node primes = YAML::Load("[2, 3, 5, 7, 11]");
+  for (std::size_t i=0;i<primes.size();i++) {
+      std::cout << primes[i].as<int>() << "\n";
+  }
+  return 0;
+}
